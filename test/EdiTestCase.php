c5b13d66d46228ec006c19839863d833818233c5
Brandon Antonio Lorenzo <bantonio@gtglobal.com>
2024-06-21 10:05:32 -0600

    Implementación de pruebas unitarias
    - Se instaló PHPUnit e Infection para implementar pruebas unitarias y de mutación.

---
+++ test/EdiTestCase.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Gtlogistics\X12Parser\Test;
+
+use PHPUnit\Framework\TestCase;
+use function Safe\file_get_contents;
+
+class EdiTestCase extends TestCase
+{
+    protected function loadFixture(string $fixture): string
+    {
+        return file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'Fixtures' . DIRECTORY_SEPARATOR . $fixture);
+    }
+
+    protected function assertDate(string $expected, mixed $value): void
+    {
+        $this->assertInstanceOf(\DateTimeInterface::class, $value);
+        $this->assertSame($expected, $value->format('Y-m-d'));
+    }
+
+    protected function assertTime(string $expected, mixed $value): void
+    {
+        $this->assertInstanceOf(\DateTimeInterface::class, $value);
+        $this->assertSame($expected, $value->format('H:i'));
+    }
+
+    protected function assertEnum(\BackedEnum $expected, mixed $value): void
+    {
+        $this->assertInstanceOf($expected::class, $value);
+        $this->assertSame($expected, $value);
+    }
+}
---
+++ test/Unit/Parser/X12ParserTest.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Gtlogistics\X12Parser\Test\Unit\Parser;
+
+use Gtlogistics\X12Parser\Edi;
+use Gtlogistics\X12Parser\Exception\MalformedX12Exception;
+use Gtlogistics\X12Parser\Heading\GsHeading;
+use Gtlogistics\X12Parser\Heading\IsaHeading;
+use Gtlogistics\X12Parser\Model\AbstractSegment;
+use Gtlogistics\X12Parser\Model\ReleaseInterface;
+use Gtlogistics\X12Parser\Parser\X12Parser;
+use Gtlogistics\X12Parser\Test\EdiTestCase;
+use Gtlogistics\X12Parser\Test\Stub\TransactionSetStub;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
+
+#[CoversClass(X12Parser::class)]
+#[UsesClass(Edi::class)]
+#[UsesClass(IsaHeading::class)]
+#[UsesClass(GsHeading::class)]
+#[UsesClass(AbstractSegment::class)]
+class X12ParserTest extends EdiTestCase
+{
+    private X12Parser $parser;
+
+    public function setUp(): void
+    {
+        $release = $this->createMock(ReleaseInterface::class);
+        $release->method('getTransactionSetClass')->willReturn(TransactionSetStub::class);
+
+        $this->parser = new X12Parser([
+            '00000' => $release,
+        ]);
+    }
+
+    public function testEmptyEdi(): void
+    {
+        $this->expectException(MalformedX12Exception::class);
+        $this->expectExceptionMessage('The first 3 bytes must be "ISA".');
+
+        $this->parser->parse($this->loadFixture('empty-edi.edi'));
+    }
+
+    public function testTrimmedIsaHeading(): void
+    {
+        $this->expectException(MalformedX12Exception::class);
+        $this->expectExceptionMessage('The ISA Header must be 106 characters long.');
+
+        $this->parser->parse($this->loadFixture('trimmed-isa-heading.edi'));
+    }
+
+    public function testReleaseNotExist(): void
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('Could not found release 00100.');
+
+        $this->parser->parse($this->loadFixture('release-not-exist.edi'));
+    }
+
+    public function testInconsistentElementSeparator(): void
+    {
+        $this->expectException(MalformedX12Exception::class);
+        $this->expectExceptionMessage('The ISA Header must be 17 elements long.');
+
+        $this->parser->parse($this->loadFixture('inconsistent-element-separator-isa-heading.edi'));
+    }
+
+    public function testNoIsaHeading(): void
+    {
+        $this->expectException(MalformedX12Exception::class);
+        $this->expectExceptionMessage('The IEA trailer does not have a matching ISA segment');
+
+        $this->parser->parse($this->loadFixture('no-isa-heading.edi'));
+    }
+
+    public function testNoIeaTrailer(): void
+    {
+        $this->expectException(MalformedX12Exception::class);
+        $this->expectExceptionMessage('The ISA header does not have the termination IEA');
+
+        $this->parser->parse($this->loadFixture('no-iea-trailer.edi'));
+    }
+
+    public function testNoGsHeading(): void
+    {
+        $this->expectException(MalformedX12Exception::class);
+        $this->expectExceptionMessage('The GE trailer does not have a matching GS segment');
+
+        $this->parser->parse($this->loadFixture('no-gs-heading.edi'));
+    }
+
+    public function testNoGeTrailer(): void
+    {
+        $this->expectException(MalformedX12Exception::class);
+        $this->expectExceptionMessage('The GS header does not have the GE trailer');
+
+        $this->parser->parse($this->loadFixture('no-ge-trailer.edi'));
+    }
+
+    public function testNoStHeading(): void
+    {
+        $this->expectException(MalformedX12Exception::class);
+        $this->expectExceptionMessage('The SE trailer does not have a matching ST segment');
+
+        $this->parser->parse($this->loadFixture('no-st-heading.edi'));
+    }
+
+    public function testNoSeTrailer(): void
+    {
+        $this->expectException(MalformedX12Exception::class);
+        $this->expectExceptionMessage('The ST header does not have the SE trailer');
+
+        $this->parser->parse($this->loadFixture('no-se-trailer.edi'));
+    }
+
+    public function testNoMatchingGsCount(): void
+    {
+        $this->expectException(MalformedX12Exception::class);
+        $this->expectExceptionMessage('The ISA header does not have the declared number of GS in the IEA trailer');
+
+        $this->parser->parse($this->loadFixture('no-matching-gs-count.edi'));
+    }
+
+    public function testNoMatchingStCount(): void
+    {
+        $this->expectException(MalformedX12Exception::class);
+        $this->expectExceptionMessage('The GS header does not have the declared number of ST in the GE trailer');
+
+        $this->parser->parse($this->loadFixture('no-matching-st-count.edi'));
+    }
+
+    public function testNoMatchingSegmentCount(): void
+    {
+        $this->expectException(MalformedX12Exception::class);
+        $this->expectExceptionMessage('The ST header does not have the declared number of segments in the SE trailer');
+
+        $this->parser->parse($this->loadFixture('no-matching-segment-count.edi'));
+    }
+}
